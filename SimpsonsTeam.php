fed147083975f1341c970ea75c15273aab73f57c
Matthew Weingarten <matthewben5@aol.com>
2017-03-15 20:57:41 -0400

    Admin Functionality

--- SimpsonsTeam.php
+++ SimpsonsTeam.php
@@ -51,7 +51,6 @@
     body {
         background-color: black;
     }
-
     /* simpsons style font */
     @font-face {
         font-family: 'simpsonsFont';
@@ -219,7 +218,8 @@
                     <li><a href="SimpsonsRetrieval.php">Retrieval</a></li>
                     <li><a href="SimpsonsExplore.php">Explore</a></li> 
                     <li class="active"><a href="#">Team</a></li> 
-                    <li><a href="SimpsonsInspiration.php">Inspiration</a></li> 
+                    <li><a href="SimpsonsInspiration.php">Inspiration</a></li>
+		    <li><a href="SimpsonsAdmin.php">Admin</a></li> 
                 </ul>
             </div>
         </div>
