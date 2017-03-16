2db56901d057c56f8f12616cc05b3fd1578a4588
Matthew Weingarten <matthewben5@aol.com>
2017-03-16 18:47:05 -0400

    Admin Functionality

--- insertSimpsonsTuples.php
+++ insertSimpsonsTuples.php
@@ -7,16 +7,13 @@
 	$type = $_GET['type'];
 	if($type == 'Character')
 	{
-		echo "Here";
 		$id = $_GET['id'].intval();
-		echo $id;
-		/* $name = $_GET['name'];
+		$name = $_GET['name'];
 		$gender = $_GET['gender'];
 		$query = "INSERT INTO CHARACTER VALUES (. $id ., ". $name .", ". $gender .");";
 		echo $query;
-		*/
 	}
-	else if ($type == 'Episode')
+	if ($type == 'Episode')
 	{
 		$id = $_GET['id'].intval();
 		$title = $_GET['title'];
@@ -31,7 +28,7 @@
 		$query = "INSERT INTO EPISODE VALUES (. $id ., ". $title .", ". $date .", . $season ., . $numberinseason ., . $numberinseries ., . $viewers ., . $rating ., ". $stillurl . ", ". $videourl . ");";
 
 	}
-	else if ($type == 'Location')
+	if ($type == 'Location')
 	{
 		$id = $_GET['id'].intval();
 		$name = $_GET['name'];
@@ -54,6 +51,8 @@
 		$wordcount = $_GET['wordcount'].intval();
 		$query = "INSERT INTO SCRIPT_LINE VALUES (. $id ., . $episodeid ., . $linenumber ., ". $rawtext .", . $timestamp ., ". $speakingline .", . $characterid ., . $locationid ., ". $character .", ". $location .", ". $spokenword .", . $wordcount .);";
 	}
+	$statement = oci_parse($connection, $query);
+	oci_execute($statement);
 	echo '<font size = "2" color = "yellow">Record inserted</font>"; 
 	oci_free_statement($statement);
 	oci_close($connection);
