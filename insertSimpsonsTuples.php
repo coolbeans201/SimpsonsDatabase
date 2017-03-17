8e1b6fabd3b710f6fc44a4988c44c9bed1a91ac2
Matthew Weingarten <matthewben5@aol.com>
2017-03-17 06:36:33 -0400

    Successfully working

--- insertSimpsonsTuples.php
+++ insertSimpsonsTuples.php
@@ -1,59 +1,59 @@
 #!/usr/local/bin/php
 <?php
+
 	$connection = oci_connect($username = 'weingart', $password = 'bridgeoverlord201', $connection_string = '//oracle.cise.ufl.edu/orcl');						  
 	if (!$connection) {
 		die('Could not connect');
 	}
 	$type = $_GET['type'];
-	if($type == 'Character')
+	if($type == "Character")
 	{
-		$id = $_GET['id'].intval();
+		$id = $_GET['id'];
 		$name = $_GET['name'];
 		$gender = $_GET['gender'];
-		$query = "INSERT INTO CHARACTER VALUES (". $id .", '". $name ."', '". $gender ."');";
-		echo $query;
+		$query = "INSERT INTO CHARACTER VALUES (". $id .", '". $name ."', '". $gender ."')";
 	}
-	if ($type == 'Episode')
+	else if ($type == "Episode")
 	{
-		$id = $_GET['id'].intval();
+		$id = $_GET['id'];
 		$title = $_GET['title'];
 		$date = $_GET['date'];
-		$season = $_GET['season'].intval();
-		$numberinseason = $_GET['numberinseason'].intval();
-		$numberinseries = $_GET['numberinseries'].intval();
-		$viewers = $_GET['viewers'].floatval();
-		$rating = $_GET['rating'].floatval();
+		$season = $_GET['season'];
+		$numberinseason = $_GET['numberinseason'];
+		$numberinseries = $_GET['numberinseries'];
+		$viewers = $_GET['viewers'];
+		$rating = $_GET['rating'];
 		$stillurl = $_GET['stillurl'];
 		$videourl = $_GET['videourl'];
-		$query = "INSERT INTO EPISODE VALUES (". $id .", '". $title ."', '". $date ."', ". $season .", ". $numberinseason .", ". $numberinseries .", ". $viewers .", ". $rating .", '". $stillurl . "', '". $videourl . "');";
+		$query = "INSERT INTO EPISODE VALUES (". $id .", '". $title ."', '". $date ."', ". $season .", ". $numberinseason .", ". $numberinseries .", ". $viewers .", ". $rating .", '". $stillurl . "', '". $videourl . "')";
 
 	}
-	if ($type == 'Location')
+	else if ($type == "Location")
 	{
-		$id = $_GET['id'].intval();
+		$id = $_GET['id'];
 		$name = $_GET['name'];
-		$query = "INSERT INTO LOCATION VALUES (". $id .", '". $name ."');";
+		$query = "INSERT INTO LOCATION VALUES (". $id .", '". $name ."')";
 
 	}
 	else
 	{
-		$id = $_GET['id'].intval();
-		$episodeid = $_GET['episodeid'].intval();
-		$linenumber = $_GET['linenumber'].intval();
+		$id = $_GET['id'];
+		$episodeid = $_GET['episodeid'];
+		$linenumber = $_GET['linenumber'];
 		$rawtext = $_GET['rawtext'];
-		$timestamp = $_GET['timestamp'].intval();
+		$timestamp = $_GET['timestamp'];
 		$speakingline = $_GET['speakingline'];
-		$characterid = $_GET['characterid'].intval();
-		$locationid = $_GET['locationid'].intval();
+		$characterid = $_GET['characterid'];
+		$locationid = $_GET['locationid'];
 		$character = $_GET['character'];
 		$location = $_GET['location'];
 		$spokenword = $_GET['spokenword'];
-		$wordcount = $_GET['wordcount'].intval();
-		$query = "INSERT INTO SCRIPT_LINE VALUES (". $id .", ". $episodeid .", ". $linenumber .", '". $rawtext ."', ". $timestamp .", '". $speakingline ."', ". $characterid .", ". $locationid .", '". $character ."', '". $location ."', '". $spokenword ."', ". $wordcount .");";
+		$wordcount = $_GET['wordcount'];
+		$query = "INSERT INTO SCRIPT_LINE VALUES (". $id .", ". $episodeid .", ". $linenumber .", '". $rawtext ."', ". $timestamp .", '". $speakingline ."', ". $characterid .", ". $locationid .", '". $character ."', '". $location ."', '". $spokenword ."', ". $wordcount .")";
 	}
 	$statement = oci_parse($connection, $query);
 	oci_execute($statement);
-	echo '<font size = "2" color = "yellow">Record inserted</font>"; 
+	echo '<font size = "2" color = "yellow">Record inserted</font>'; 
 	oci_free_statement($statement);
 	oci_close($connection);
 ?>
